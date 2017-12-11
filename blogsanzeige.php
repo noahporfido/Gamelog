daf68b5383d15cfa27fefb575120360ef82a841c
noahporfido <noah.porfido@icloud.com>
2017-12-11 19:50:59 +0100

    dass
    djkajksdjhkas

--- blogsanzeige.php
+++ blogsanzeige.php
@@ -2,35 +2,38 @@
 $blogBeiträge = getEntries($blogId);
 $blogOwners = getUserNames();
 $eid;
-
-echo "<div id='BlogInhalt'>";
-
-
-echo "<div id='blogAnzeige'>";
-
-foreach($blogOwners as $blogOwner)
-{
-    if($blogId == $blogOwner['uid'])
-    {
-    echo "<h1 id='blogOwner'>".$blogOwner['name']."'s Blog</h1>";
-    }
-}
-
-  echo "<div id='blogTitelMenu'>";
-    echo "<ul>";
+    
 foreach($blogBeiträge as $blogBeitrag)
 {
     $replace = str_replace(array("\r\n","\r","\n"),"<br/>", $blogBeitrag['content']);
+    $date = date("Y-m-d H:i:s", $blogBeitrag['datetime']);
     
-    
-    
-    echo "<a href='index.php?function=einbloganzeigen&bid=".$blogId."&eid=".$blogBeitrag['eid']. "&title='BlogAuswählen' class='blogTitel'>".$blogBeitrag['title']."</a>";
+    echo "<a href='index.php?function=blogsanzeige&bid=".$blogId."&eid=".$blogBeitrag['eid']. "&title=BlogAuswählen' class='blogTitel'>".$blogBeitrag['title']."
+    ".$date."</a><br>";
    
 }
-    echo "</ul>";
-    echo "</div>";
+
+echo "<div id='blogTeil'>";
+
+    if(isset($_GET['eid']))
+    {
+ $eid= $_GET['eid'];
+        
+$blogbeiträge = getEntry($eid);
+
+    $replace = str_replace(array("\r\n","\r","\n"),"<br/>", $blogbeiträge['content']);
+    echo "<div id='blogText'>";
+    $date = date("Y-m-d H:i:s", $blogbeiträge['datetime']);
+    echo "<p>".$blogbeiträge['title']."</p>
+    <p id='blogDate'>".$date."</p>
+    <p>".$replace."</p>
+    </div>";
+    }
+else
+{
+    echo "<p>Wählen sie einen Blog</p>";
+}
 
 echo "</div>";
-
-
 ?>
+
